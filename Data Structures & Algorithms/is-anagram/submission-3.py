eb7decddb50ee0584c2a5c94997adf28646061ad
Illeyan Atif <154643275+Illey1@users.noreply.github.com>
2026-09-04 22:02:18 -0700

    Add: is-anagram - submission-3

---
+++ Data Structures & Algorithms/is-anagram/submission-3.py
@@ -0,0 +1,23 @@
+class Solution:
+    def isAnagram(self, s: str, t: str) -> bool:
+        if (len(s) != len(t)):
+            return False
+        
+        count = {}
+
+        for i in s:
+            if i in count:
+                count[i] += 1
+            else:
+                count[i] = 1
+        
+        for i in t:
+            if i in count:
+                count[i] -= 1
+            else:
+                return False
+
+            if (count[i] < 0):
+                return False
+        
+        return True
